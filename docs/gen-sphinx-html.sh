7c1b67d9281427d0cd5d0b957060cdc39b600e93
Gary Kwong [:gkw] <nth10sd@gmail.com>
2020-08-17 01:32:10 -0700

    More Sphinx support fixes.

--- docs/gen-sphinx-html.sh
+++ docs/gen-sphinx-html.sh
@@ -1,7 +1,8 @@
 #! /bin/bash
 
-# Boostrap Sphinx
-sphinx-quickstart --sep -p "ocs" -a "FOO" -r "BAR" -l "en" --ext-autodoc --no-makefile --no-batchfile --no-use-make-mode;
+# Bootstrap Sphinx
+sphinx-quickstart --sep -p "ocs" -a "FOO" -r "BAR" -l "en" \
+    --ext-autodoc --no-makefile --no-batchfile --no-use-make-mode;
 
 # Copy over the intended copy of conf.py
 cp source/conf_correct.py source/conf.py;
@@ -9,8 +10,9 @@ cp source/conf_correct.py source/conf.py;
 # Generate Sphinx documentation
 sphinx-apidoc -o source/ ../ocs/;
 
-# Add "modules" to one of the sections in index.rst in the middle of several empty lines
-cat source/index.rst | tr '\n' '\f' | sed 's/\f\f\f\f/\f\f   modules\f\f/' | tr '\f' '\n' > source/index_new.rst;
+# Add "modules" to portion of index.rst in the middle of several empty lines
+tr '\n' '\f' < source/index.rst | \
+    sed 's/\f\f\f\f/\f\f   modules\f\f/' | tr '\f' '\n' > source/index_new.rst;
 mv source/index_new.rst source/index.rst;
 
 # Generate Sphinx HTML documentation
--- docs/source/conf_correct.py
+++ docs/source/conf_correct.py
@@ -1,3 +1,5 @@
+# pylint: disable=invalid-name,missing-module-docstring,wrong-import-position
+
 # Configuration file for the Sphinx documentation builder.
 #
 # This file only contains a selection of the most common options. For a full
@@ -12,14 +14,16 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../..'))
 
-import ocs
+
+sys.path.insert(0, os.path.abspath("../.."))
+
+import ocs  # noqa: E402,I001
 
 # -- Project information -----------------------------------------------------
 
 project = ocs.__title__
-copyright = ocs.__copyright__
+copyright = ocs.__copyright__  # pylint: disable=redefined-builtin
 author = ocs.__author__
 
 # The full version, including alpha/beta/rc tags
@@ -29,19 +33,19 @@ release = ocs.__version__
 # -- General configuration ---------------------------------------------------
 
 # Add any Sphinx extension module names here, as strings. They can be
-# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
+# extensions coming with Sphinx (named "sphinx.ext.*") or your custom
 # ones.
 extensions = [
     "sphinx.ext.autodoc",
 ]
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ["_templates"]
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = []
+exclude_patterns = []  # type: ignore
 
 
 # -- Options for HTML output -------------------------------------------------
@@ -49,9 +53,9 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = "alabaster"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ["_static"]
