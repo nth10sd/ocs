de68ae2897a0aaa213effaad7220d3460b954823
Gary Kwong [:gkw] <nth10sd@gmail.com>
2021-01-12 19:20:26 -0800

    Use .strip() to remove whitespaces before and after intended string, instead of merely .rstrip()

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ VERSION_INDICATOR = "__version__"  # This sets the version in INIT_FILE
 with open((Path(MODULE_NAME) / INIT_FILE).expanduser().resolve(), "r") as f:  # Look in module's __init__ for __version__
     for line in f:
         if line.startswith(VERSION_INDICATOR):
-            MODULE_VER = line.split("=", 1)[1].rstrip()[1:-1]  # Excl start/end quotes, use remove[pre|suf]fix on Python 3.9+
+            MODULE_VER = line.split("=", 1)[1].strip()[1:-1]  # Excl start/end quotes, use remove[pre|suf]fix on Python 3.9+
             break
     if not MODULE_VER:
         raise ValueError(f"{VERSION_INDICATOR} is not defined in {MODULE_NAME}/{INIT_FILE}")
