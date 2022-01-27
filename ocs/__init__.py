dc8d42565beaa1f488762b0c8dd19f1670a22d3a
Gary Kwong [:gkw] <nth10sd@gmail.com>
2022-01-27 00:11:33 -0800

    Bump version number

--- ocs/__init__.py
+++ ocs/__init__.py
@@ -5,7 +5,7 @@
 """Module details."""
 
 __title__ = "ocs"
-__version__ = "0.9.0"
+__version__ = "0.10.0a1"
 __author__ = "Gary Kwong"
 __license__ = "MPL 2.0"
 __copyright__ = "Copyright 2020-2022 Gary Kwong"
