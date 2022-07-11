ffd92d4f6ad614f639b18cd737d6360a82aad42f
Gary Kwong [:gkw] <nth10sd@gmail.com>
2022-07-10 23:27:55 -0700

    Update docstring for constants

--- ocs/util/constants.py
+++ ocs/util/constants.py
@@ -2,7 +2,7 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
-"""Allows SpiderMonkey shell inspection to ensure that it is compiled as intended."""
+"""Constants used throughout."""
 
 from __future__ import annotations
 
