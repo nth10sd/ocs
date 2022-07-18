9c13a8262bb70cc57bb466f9decb4b5d5b380bac
Gary Kwong [:gkw] <nth10sd@gmail.com>
2022-07-18 00:46:42 -0700

    Rename variable to point to args namespace instead

--- tests/spidermonkey/test_parsing.py
+++ tests/spidermonkey/test_parsing.py
@@ -9,23 +9,23 @@ from ocs.spidermonkey.parsing import parse_args
 
 def test_parser() -> None:
     """Test the parser."""
-    parser = parse_args(
+    args = parse_args(
         [
             '-b="--enable-debug --enable-simulator=arm64"',
             "-r",
             "a5301180315c5a152c4173e6fc741e02f271d4ed",
         ]
     )
-    assert parser.build_opts == "--enable-debug --enable-simulator=arm64"
-    assert parser.revision == "a5301180315c5a152c4173e6fc741e02f271d4ed"
+    assert args.build_opts == "--enable-debug --enable-simulator=arm64"
+    assert args.revision == "a5301180315c5a152c4173e6fc741e02f271d4ed"
 
-    parser2 = parse_args(['--build-opts="--enable-address-sanitizer"'])
-    assert parser2.build_opts == "--enable-address-sanitizer"
-    assert not parser2.revision
+    args2 = parse_args(['--build-opts="--enable-address-sanitizer"'])
+    assert args2.build_opts == "--enable-address-sanitizer"
+    assert not args2.revision
 
-    parser3 = parse_args([])
-    assert not parser3.build_opts
-    assert not parser3.revision
+    args3 = parse_args([])
+    assert not args3.build_opts
+    assert not args3.revision
 
 
 def test_parser_no_equals() -> None:
