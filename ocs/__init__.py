983c7d15c5220fe3c7e0bfc62cd1b64eed43cbd3
Gary Kwong [:gkw] <nth10sd@gmail.com>
2022-09-02 02:31:57 -0700

    Big changes coming

--- ocs/__init__.py
+++ ocs/__init__.py
@@ -1,11 +1,7 @@
-# This Source Code Form is subject to the terms of the Mozilla Public
-# License, v. 2.0. If a copy of the MPL was not distributed with this
-# file, You can obtain one at https://mozilla.org/MPL/2.0/.
-
 """Module details."""
 
 __title__ = "ocs"
 __version__ = "0.10.0a1"
 __author__ = "Gary Kwong"
-__license__ = "MPL 2.0"
+__license__ = "All Rights Reserved"
 __copyright__ = "Copyright 2020-2022 Gary Kwong"
