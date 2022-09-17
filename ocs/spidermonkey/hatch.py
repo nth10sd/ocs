f0a1c3421f105e16223881bc1eaddb5916695186
Gary Kwong [:gkw] <nth10sd@gmail.com>
2022-09-16 22:18:00 -0700

    Remove attempts at MozillaBuild 3.4 compatibility

--- ocs/spidermonkey/hatch.py
+++ ocs/spidermonkey/hatch.py
@@ -123,15 +123,6 @@ def configure_js_shell_compile(shell: SMShell) -> None:
     js_objdir_path.mkdir()
     shell.js_objdir = js_objdir_path
 
-    # Run autoconf 2.13 only if repository revision is before:
-    #   m-c rev 633690:c5dc125ea32ba3e9a7c3fe3cf5be05abd17013a3, Fx106
-    # See bug 1787977. m-c rev has been bumped to account for known broken ranges
-    if zzconstants.IS_MOZILLABUILD_3_OR_OLDER and hg_helpers.exists_and_is_ancestor(
-        shell.build_opts.repo_dir,
-        shell.hg_hash,
-        "parents(c5dc125ea32ba3e9a7c3fe3cf5be05abd17013a3)",
-    ):
-        utils.autoconf_run(shell.build_opts.repo_dir / "js" / "src")
     configure_binary(shell)
     sm_compile(shell)
     verify_binary(shell)
@@ -184,19 +175,6 @@ def configure_binary(  # pylint: disable=too-complex,too-many-branches
         and parse(platform.mac_ver()[0]) >= parse("10.13")
         and not shell.build_opts.enable32
     ):
-        # Add the AUTOCONF env variable if repository revision is before:
-        #   m-c rev 633690:c5dc125ea32ba3e9a7c3fe3cf5be05abd17013a3, Fx106
-        # See bug 1787977. m-c rev has been bumped to account for known broken ranges
-        if (
-            zzconstants.IS_MOZILLABUILD_3_OR_OLDER
-            and shutil.which("brew")
-            and hg_helpers.exists_and_is_ancestor(
-                shell.build_opts.repo_dir,
-                shell.hg_hash,
-                "parents(c5dc125ea32ba3e9a7c3fe3cf5be05abd17013a3)",
-            )
-        ):
-            cfg_env["AUTOCONF"] = "/usr/local/Cellar/autoconf213/2.13/bin/autoconf213"
         cfg_cmds.append("sh")
         cfg_cmds.append(str(shell.js_cfg_path))
         if shell.build_opts.enableSimulatorArm64:
@@ -214,9 +192,6 @@ def configure_binary(  # pylint: disable=too-complex,too-many-branches
             raise FileNotFoundError(
                 f"llvm-config.exe not found at: {win_mozbuild_clang_bin_path}",
             )
-        if zzconstants.IS_MOZILLABUILD_3_OR_OLDER:
-            cfg_env["LIBCLANG_PATH"] = str(win_mozbuild_clang_bin_path)
-            cfg_env["MAKE"] = "mozmake"  # Workaround for bug 948534
         if shell.build_opts.enableAddressSanitizer:
             cfg_env["LDFLAGS"] = (
                 "clang_rt.asan_dynamic-x86_64.lib "
@@ -296,25 +271,6 @@ def configure_binary(  # pylint: disable=too-complex,too-many-branches
         cfg_cmds.append("--with-ccache")
     cfg_cmds.append("--enable-gczeal")
     cfg_cmds.append("--enable-debug-symbols")  # gets debug symbols on opt shells
-    # Use --disable-bootstrap if repository revision is on/after:
-    #   m-c rev 563494:c69826ee93e91671fd5c87f01ac7bd2b483c991f, Fx86
-    # but before:
-    #   m-c rev 633690:c5dc125ea32ba3e9a7c3fe3cf5be05abd17013a3, Fx106
-    # See bug 1787977. m-c rev has been bumped to account for known broken ranges
-    if (
-        zzconstants.IS_MOZILLABUILD_3_OR_OLDER
-        or hg_helpers.exists_and_is_ancestor(
-            shell.build_opts.repo_dir,
-            shell.hg_hash,
-            "parents(c5dc125ea32ba3e9a7c3fe3cf5be05abd17013a3)",
-        )
-        and not hg_helpers.exists_and_is_ancestor(
-            shell.build_opts.repo_dir,
-            shell.hg_hash,
-            "parents(c69826ee93e91671fd5c87f01ac7bd2b483c991f)",
-        )
-    ):
-        cfg_cmds.append("--disable-bootstrap")
     cfg_cmds.append("--disable-tests")
 
     if (
