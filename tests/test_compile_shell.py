efc4a438ff7a4edf92825d4ac6366756498b5f27
Gary Kwong [:gkw] <nth10sd@gmail.com>
2023-04-05 00:50:41 -0700

    Workaround a test issue due to a not-yet-released dependency version

--- tests/test_compile_shell.py
+++ tests/test_compile_shell.py
@@ -7,6 +7,8 @@ from functools import cache
 import os
 from pathlib import Path
 import platform
+import shutil
+import subprocess
 
 import pytest
 from zzbase.patching.common import patch_files
@@ -37,6 +39,24 @@ def test_shell_compile() -> Path:
         "Monkeypatching coverage rev"
         not in (VENV_SITE_PKGS / "coverage" / "inorout.py").read_text()
     ):
+        # Start temporary section until https://bit.ly/3Kf6gjL gets released ###
+        subprocess.run(
+            (["sd"] if bool(shutil.which("sd")) else ["sed", "-i"])
+            + [
+                r'if original_filename.startswith\("<"\):',
+                "if original_filename.startswith('<'):",
+                (
+                    VENV_SITE_PKGS
+                    / "zzbase"
+                    / "data"
+                    / "pypi_library_patches"
+                    / "coverage"
+                    / "64130ee5-patch-for-m-c-to-work.diff"
+                ),
+            ],
+            check=True,
+        )
+        # End temporary section until https://bit.ly/3Kf6gjL gets released ###
         patch_files(  # Do not assert, as we do not care if patch is already applied
             VENV_SITE_PKGS,
             (
@@ -49,11 +69,10 @@ def test_shell_compile() -> Path:
             ),
             1,
         )
-    assert (
+    assert (  # If this assert fails, try removing temporary section above
         "Monkeypatching coverage rev"
         in (VENV_SITE_PKGS / "coverage" / "inorout.py").read_text()  # Re-read again
     )
-
     default_parameters_debug = (
         "--enable-debug --disable-optimize --enable-oom-breakpoint --without-intl-api"
     )
