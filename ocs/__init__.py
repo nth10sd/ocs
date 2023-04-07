3bdce6298fe610b70d14e45d9f08c25bbbb160b1
Gary Kwong [:gkw] <nth10sd@gmail.com>
2023-04-06 23:22:21 -0700

    Bump version number

--- ocs/__init__.py
+++ ocs/__init__.py
@@ -1,7 +1,7 @@
 """Module details."""
 
 __title__ = "ocs"
-__version__ = "0.10.0"
+__version__ = "0.11.0a1"
 __author__ = "Gary Kwong"
 __license__ = "All Rights Reserved"
 __copyright__ = "Copyright 2020-2023 Gary Kwong"
