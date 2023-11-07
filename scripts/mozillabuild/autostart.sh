29529d4cc5cb0b3d6949cd18d50a4cbbe150e0d2
Gary Kwong [:gkw] <nth10sd@gmail.com>
2023-11-06 21:36:57 -0800

    Bump more tool versions

--- scripts/mozillabuild/autostart.sh
+++ scripts/mozillabuild/autostart.sh
@@ -37,17 +37,17 @@ if [ "$BUILDSM" == "--enable-address-sanitizer" ] ; then
 fi ;
 
 echo "=== pytest attempt: 1 ===" ;
-if ! BUILDSM="$*" python -u -m pytest --black --cov --mypy --pylint --ruff ; then
+if ! BUILDSM="$*" python -u -m pytest --cov --mypy --pylint --ruff --ruff-format ; then
     echo "=== pytest attempt: 2 ===" ;
     echo "=== Removing shell-cache ... ===" ;
     rm -rf "$HOME"/shell-cache/ ;
     echo "=== Removed shell-cache successfully! ===" ;
-    if ! BUILDSM="$*" python -u -m pytest --black --cov --mypy --pylint --ruff ; then
+    if ! BUILDSM="$*" python -u -m pytest --cov --mypy --pylint --ruff --ruff-format ; then
         echo "=== pytest attempt: 3 ===" ;
         echo "=== Removing shell-cache ... ===" ;
         rm -rf "$HOME"/shell-cache/ ;
         echo "=== Removed shell-cache successfully! ===" ;
-        if ! BUILDSM="$*" python -u -m pytest --black --cov --mypy --pylint --ruff ;
+        if ! BUILDSM="$*" python -u -m pytest --cov --mypy --pylint --ruff --ruff-format ;
         then
             date > "$HOME"/pytest-failure.txt ;
         fi ;
