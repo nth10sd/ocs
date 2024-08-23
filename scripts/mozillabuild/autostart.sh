f816121ede925e2eb7eaf15e376aecbc80fe8496
Gary Kwong [:gkw] <nth10sd@gmail.com>
2024-08-22 21:20:12 -0700

    Update settings to make things work on CI

--- scripts/mozillabuild/autostart.sh
+++ scripts/mozillabuild/autostart.sh
@@ -7,7 +7,7 @@ echo "BUILDSM env variable is: $BUILDSM" ;
 python -u -m pip install --upgrade pip setuptools wheel ;
 pushd /d/a/ocs/ocs/ || exit ;
 
-openssl enc -base64 -d <<< SG9zdCAqCkFkZEtleXNUb0FnZW50IHllcwpTdHJpY3RIb3N0S2V5Q2hlY2tpbmcgbm8KCkhvc3QgenpiYXNlLmdpdGh1Yi5jb20KSG9zdE5hbWUgZ2l0aHViLmNvbQpVc2VyIGdpdApJZGVudGl0eUZpbGUgfi8uc3NoL2lkX2VkMjU1MTlfUk9fenpiYXNlCklkZW50aXRpZXNPbmx5IHllcwo= > "$HOME"/.ssh/config ;
+openssl enc -base64 -d <<< SG9zdCAqCkFkZEtleXNUb0FnZW50IHllcwpTdHJpY3RIb3N0S2V5Q2hlY2tpbmcgbm8KCkhvc3QgY3Jhc2gyY292LmdpdGh1Yi5jb20KSG9zdE5hbWUgZ2l0aHViLmNvbQpVc2VyIGdpdApJZGVudGl0eUZpbGUgfi8uc3NoL2lkX2VkMjU1MTlfUk9fY3Jhc2gyY292CklkZW50aXRpZXNPbmx5IHllcwoKSG9zdCB6emJhc2UuZ2l0aHViLmNvbQpIb3N0TmFtZSBnaXRodWIuY29tClVzZXIgZ2l0CklkZW50aXR5RmlsZSB+Ly5zc2gvaWRfZWQyNTUxOV9ST196emJhc2UKSWRlbnRpdGllc09ubHkgeWVzCg== > "$HOME"/.ssh/config ;
 python -u -m pip install -r requirements.txt ;
 python -u -m pip install --upgrade -e . ;
 
