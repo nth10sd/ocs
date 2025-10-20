6e23a1b0a28e7a4c2a74290950f3a6b849700bbc
Gary Kwong [:gkw] <nth10sd@gmail.com>
2025-10-20 03:14:56 -0700

    Comment out unused code for now, instead of just disabling for code coverage

--- ocs/util/constants.py
+++ ocs/util/constants.py
@@ -1,8 +1,8 @@
 """Constants used throughout."""
 
-from __future__ import annotations
+# \from __future__ import annotations
 
-from typing import Final
+# \from typing import Final
 
-# Ignored (code coverage), as this is boilerplate for now
-PACKAGE_NAME: Final = __name__.split(".", maxsplit=1)[0]  # pragma: no cover
+# \Commented out, as this is boilerplate for now
+# \PACKAGE_NAME: Final = __name__.split(".", maxsplit=1)[0]
